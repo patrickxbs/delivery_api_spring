11ad28e7faa44caf01c5a20178ad4012d2a4700b
Emanuel Patrick <emanuelpatrick440@gmail.com>
2026-02-26 11:24:53 -0300

    testando

--- src/test/java/com/patrick/delivery/DeliveryApplicationTests.java
+++ src/test/java/com/patrick/delivery/DeliveryApplicationTests.java
@@ -2,8 +2,10 @@ package com.patrick.delivery;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DeliveryApplicationTests {
 
 	@Test
